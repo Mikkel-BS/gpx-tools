db72dc45dfa3d4aeeb40ad4626de0fbd58fed775
Mikkel-BS <mikkel.brudvik.sanderud@gmail.com>
2026-09-11 16:16:20 +0200

    Add NTR1 CRC32 primitive

---
+++ src/ntr1/crc32.ts
@@ -0,0 +1,14 @@
+export function crc32(bytes: Uint8Array): number {
+  let crc = 0xffffffff;
+  for (const byte of bytes) {
+    crc ^= byte;
+    for (let bit = 0; bit < 8; bit += 1) {
+      crc = (crc >>> 1) ^ (0xedb88320 & -(crc & 1));
+    }
+  }
+  return (crc ^ 0xffffffff) >>> 0;
+}
+
+export function crc32Hex(bytes: Uint8Array): string {
+  return crc32(bytes).toString(16).toUpperCase().padStart(8, '0');
+}
